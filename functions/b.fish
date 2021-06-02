762f77175c72ea2c490a320fed29b2d43eee0bf9
François Allain <frallain@gmail.com>
2021-06-02 11:23:17 +0200

    change b

--- functions/b.fish
+++ functions/b.fish
@@ -1,3 +1,4 @@
 function b
-    git branch --all -vv
+    # git branch --all -vv
+    git for-each-ref --sort=-committerdate refs/heads/ --format='%(color: red)%(committerdate:short) %(color: black)%(refname:short)' | head -5
 end
